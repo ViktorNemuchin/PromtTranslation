fe758dc683527aa9f8f43fae3ab4dad24fea3618
nemuhin_vv <nemuhin_vv@nlmk.com>
2021-01-23 14:09:56 +0300

    Переезд на service worker

---
+++ PromtTranslation.WorkerService/Program.cs
@@ -0,0 +1,52 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Quartz.Core;
+using Quartz;
+using System.Net.Http;
+using PromtTranslation.WorkerService.Extensions;
+using PromtTranslation.Dtl.UnitOfWowrk.Interface;
+using PromtTranslation.Dtl.UnitOfWowrk.Implementation;
+using PromtTranslation.Dtl.Context;
+using PromtTranslation.Services.Implementation;
+using PromtTranslation.Services.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace PromtTranslation.WorkerService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHttpClient();
+                    services.AddScoped<ITranslationService, TranslationService>();
+                    services.AddDbContext<TranslationDbContext>(options =>
+                    {
+                        options.UseNpgsql(hostContext.Configuration.GetSection("ConnectionString").GetSection("PromtDb").Value,
+                            npsqlOptions => npsqlOptions.MigrationsAssembly("PromtTranslation.Api"));
+                        options.EnableSensitiveDataLogging();
+                    });
+                    services.AddScoped<TranslationDbContext>();
+                    services.AddScoped<ITranslatioonUnitOfWork, TranslationUnitOfWork>();
+                    services.AddQuartz(q =>
+                    {
+                        // Use a Scoped container to create jobs. I'll touch on this later
+                        q.UseMicrosoftDependencyInjectionScopedJobFactory();
+                        q.AddJobAndTrigger<PromtTranslationJob>(hostContext.Configuration);
+                    });
+                    services.AddQuartzHostedService(
+                        q => q.WaitForJobsToComplete = true
+                        );
+                });
+    }
+}
---
+++ PromtTranslation.WorkerService/PromtTranslationJob.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using PromtTranslation.Services.Interface;
+using Quartz;
+
+namespace PromtTranslation.WorkerService
+{
+    public class PromtTranslationJob : IJob
+    {
+        private readonly ILogger<PromtTranslationJob> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly ITranslationService _translationService;
+
+        public PromtTranslationJob(ILogger<PromtTranslationJob> logger, IHttpClientFactory httpClientFactory, ITranslationService translationService) 
+        {
+            _httpClientFactory = httpClientFactory;
+            _translationService = translationService;
+            _logger = logger;
+        }
+        public Task Execute(IJobExecutionContext context)
+        {
+            _translationService.TranslateText("Text");
+            _logger.LogInformation("Hello world!");
+            return Task.CompletedTask;
+        }
+    }
+}
